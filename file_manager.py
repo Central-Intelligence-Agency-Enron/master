4a0c7e83bd366a34a7eef2c4474bff3dcd476f8f
Nicolas Malgat <nicolas.malgat@hotmail.com>
2020-08-01 15:48:31 +0200

    add - programmes permettant de récupérer les path des mails

--- file_manager.py
+++ file_manager.py
@@ -1,17 +1,48 @@
-import os
+from nltk.tokenize import word_tokenize
+from nltk.probability import FreqDist
 
-def list_folder(link):
+from pathlib import Path
 
-    if not link:
-        link = r"C:\\Users\\utilisateur\\Desktop\\maildir\\"
+_maildir_path = Path("C:/Users/utilisateur/Desktop/maildir")
 
-    dirList = os.scandir(link)
-    for dir in dirList:
-        if os.path.isdir(dir) == True:
-            print(dir.path, '\t\t', dir.name)
+def mail_location(link=None):
+    # print("Indiquer l'emplacement de maildir")
+    # link = Path(input())
+    return link
+
+def get_text(file_path):
+    raise NotImplementedError
+
+def get_files(folder_path, liste_fichier=[]):
+    """ Retourne la liste des fichiers contenues dans le dossier spécifié
+
+    Args:
+        path (str): path du dossier
+        liste_fichier (list of str): liste de paths vers les fichiers
+    """
+
+    for path in folder_path.iterdir():
+        # if path.is_dir():
+        #     print("Exploration dossier ", path.name, " avec nbr elements ", len(list(path.iterdir())))
+
+        if path.is_dir():
+            get_files(path, liste_fichier)
+        elif path.is_file():
+            liste_fichier.append(path)
         else:
-            print(dir.path, " n'es pas un fichier")
+            print('truc chelou: ', )
+            continue
+
+    return liste_fichier
+
 
 if __name__ == "__main__":
-    print("Indiquer l'emplacement des mails")
-    list_folder(input())
+
+    path = mail_location(_maildir_path)
+
+    liste_fichier = get_files(path)
+
+    print(
+        "total de mails: ", len(liste_fichier)
+        )
+
