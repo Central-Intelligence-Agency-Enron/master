5a2bbd25b8ebace01598f61c65286cc6c8640e99
Nicolas Malgat <nicolas.malgat@hotmail.com>
2020-08-04 16:38:23 +0200

    made a mail parsing class

--- mail_parser.py
+++ mail_parser.py
@@ -1,8 +1,21 @@
 import email
 
 
-def parse_mail(text):
-    return email.message_from_string(text)
+class mail_parser():
+
+    def __init__(self, mail_file):
+        self.message = self._parse_mail(mail_file.read())
+        self.header = self._get_header()
+        self.content = self._get_content()
+
+    def _parse_mail(self, text):
+        return email.message_from_string(text)
+
+    def _get_header(self):
+        return self.message.items()
+
+    def _get_content(self):
+        return self.message.get_payload()
 
 
 if __name__ == '__main__':
@@ -10,8 +23,8 @@ if __name__ == '__main__':
 
     mail = Path("D:/simplon/maildir/ring-r/sent_items/42")
 
-    with open(mail, 'r') as file:
-        parsed = parse_mail(file.read())
+    # with open(mail, 'r') as file:
+    #     parsed = parse_mail(file.read())
 
-    print(parsed.get_payload())  # contenu
-    print(parsed.items())  # header
+    # print(parsed.get_payload())  # contenu
+    # print(parsed.items())  # header
