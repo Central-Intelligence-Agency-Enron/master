ac9a69af29be7b0af1eb385e29ddd4bbb115ea47
David <david.krygier1010@gmail.com>
2020-08-26 11:49:45 +0200

    modif mineures

--- impression_graph.py
+++ impression_graph.py
@@ -1,26 +1,22 @@
-import pandas as pd
-import numpy as np
-import nltk.corpus
-from nltk.tokenize import word_tokenize
-from nltk.probability import FreqDist
-from nltk.stem import WordNetLemmatizer
-from nltk.tokenize import RegexpTokenizer
-from nltk.corpus import stopwords
-import matplotlib.pyplot as plt
-
-"""Extraction des textes des fichiers csv des personnes conetnues dans liste_personnes
+""" Extraction des textes des fichiers csv des personnes conetnues dans liste_personnes
 Comptage des mots présent dans les mails et création du top 10 000 des mots
 Impression d'un graphique montrant le ratio des mots présents dans liste_mots par mail et par personne
 """
 
+import pandas as pd
+import numpy as np
+from nltk.probability import FreqDist
+from nltk.tokenize import RegexpTokenizer
+import matplotlib.pyplot as plt
+
+
 i = 0
-liste_personnes = ["kean-s", "shackleton-s", "dasovich-j", "cash-m", "farmer-d"]
-liste_mots = ["dollar", "buy", "sell", "trade", "agreement", "transaction"]
+liste_personnes = ["kean-s", "shackleton-s", "dasovich-j", "cash-m", "farmer-d", "stokley-c", "harris-s"]
+liste_mots = ["dollar", "buy", "sell", "trade", "agreement", "transaction", "exchange", "concluded"]
 liste_top = []
 data_top = []
 
 #Initialisation des fonctions
-stopW = stopwords.words('english')
 tokenizer = RegexpTokenizer(r'\w+')
 
 
@@ -31,11 +27,10 @@ for name in liste_personnes:
     for row in csv['content']:
         text += row.lower()
     liste = tokenizer.tokenize(text)
-    liste = [word for word in liste if word not in stopW]
     freq_dist = FreqDist(liste)
     top = freq_dist.most_common(10000)
 
-    data_frame = pd.DataFrame(top, columns = ['Mot', '''Nb d'apparition'''])
+    data_frame = pd.DataFrame(top, columns=['Mot', '''Nb d'apparition'''])
     liste_top.append(data_frame)
 
 
@@ -55,5 +50,5 @@ for values in data_top:
 
 lines, labels = plt.thetagrids(np.degrees(label_placement), labels=liste_mots)
 plt.legend(labels=liste_personnes)
-
 plt.show()
+
