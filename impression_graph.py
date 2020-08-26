9f3c12cba4c1b79e77deea813cb29876f69a7bf8
David <david.krygier1010@gmail.com>
2020-08-26 11:59:08 +0200

    pep8

--- impression_graph.py
+++ impression_graph.py
@@ -11,44 +11,44 @@ import matplotlib.pyplot as plt
 
 
 i = 0
-liste_personnes = ["kean-s", "shackleton-s", "dasovich-j", "cash-m", "farmer-d", "stokley-c", "harris-s"]
-liste_mots = ["dollar", "buy", "sell", "trade", "agreement", "transaction", "exchange", "concluded"]
-liste_top = []
-data_top = []
+LISTE_PERSONNES = ["kean-s", "shackleton-s", "dasovich-j", "cash-m", "farmer-d", "stokley-c", "harris-s"]
+LISTE_MOTS = ["dollar", "buy", "sell", "trade", "agreement", "transaction", "exchange", "concluded"]
+LISTE_TOP = []
+DATA_TOP = []
 
 #Initialisation des fonctions
-tokenizer = RegexpTokenizer(r'\w+')
+TOKENIZER = RegexpTokenizer(r'\w+')
 
 
-for name in liste_personnes:
+for name in LISTE_PERSONNES:
     text = ""
     liste = ""
     csv = pd.read_csv('s_user_csv/' + name + ".csv", sep=',')
     for row in csv['content']:
         text += row.lower()
-    liste = tokenizer.tokenize(text)
+    liste = TOKENIZER.tokenize(text)
     freq_dist = FreqDist(liste)
     top = freq_dist.most_common(10000)
 
     data_frame = pd.DataFrame(top, columns=['Mot', '''Nb d'apparition'''])
-    liste_top.append(data_frame)
+    LISTE_TOP.append(data_frame)
 
 
-for top in liste_top:
+for top in LISTE_TOP:
     word_values = []
-    for word in liste_mots:
+    for word in LISTE_MOTS:
         nb_word = sum(top["Nb d'apparition"][top['Mot'].str.contains(word)])/len(top)
         word_values.append(nb_word)
     word_values = np.concatenate((word_values, [word_values[0]]))
-    data_top.append(word_values)
+    DATA_TOP.append(word_values)
 
-label_placement = np.linspace(start=0, stop=2*np.pi, num=len(word_values))
+LABEL_PLACEMENT = np.linspace(start=0, stop=2*np.pi, num=len(word_values))
 plt.figure(figsize=(10, 10))
 plt.subplot(polar=True)
-for values in data_top:
-    plt.plot(label_placement, values)
+for values in DATA_TOP:
+    plt.plot(LABEL_PLACEMENT, values)
 
-lines, labels = plt.thetagrids(np.degrees(label_placement), labels=liste_mots)
-plt.legend(labels=liste_personnes)
+LINES, LABELS = plt.thetagrids(np.degrees(LABEL_PLACEMENT), labels=LISTE_MOTS)
+plt.legend(labels=LISTE_PERSONNES)
 plt.show()
 
